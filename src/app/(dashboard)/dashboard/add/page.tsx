400004323fd2cecb642274478be983bf6d44f4c6
Nathan Petain <nejc.petan14@gmail.com>
2024-10-17 00:32:02 +0200

    Add friend functionality, dashboard layout, helpers, etc

---
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -0,0 +1,13 @@
+import AddFriendButton from "@/app/components/AddFriendButton";
+import { FC } from "react";
+
+const page: FC = ({}) => {
+  return (
+    <main>
+      <h1 className="font-bold text-5xl mb-8">Add a friend</h1>
+      <AddFriendButton />
+    </main>
+  );
+};
+
+export default page;
